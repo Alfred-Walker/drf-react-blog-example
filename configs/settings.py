79b51699e34b1ddf560773ae9a9f23401e736065
Alfred-Walker <studio.alfred.walker@gmail.com>
2019-10-24 20:23:50 +0900

    Feat: enable default pagination

--- configs/settings.py
+++ configs/settings.py
@@ -134,3 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# pagination
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
