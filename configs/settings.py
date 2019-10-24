ea8af0d16b287ecf12dfe2eb04216784deccfa33
Alfred-Walker <studio.alfred.walker@gmail.com>
2019-10-24 19:17:19 +0900

    Feat: hide passwords

--- configs/settings.py
+++ configs/settings.py
@@ -9,8 +9,23 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import json
 import os
+
+from django.core.exceptions import ImproperlyConfigured
+
+with open("conf/privates.json") as f:
+    privates = json.loads(f.read())
+
+
+# keep secret keys in secrets.json
+def get_private(key, privates_json=privates):
+    try:
+        return privates_json[key]
+    except KeyError:
+        error_msg = "Set the {0} environment variable".format(key)
+        raise ImproperlyConfigured(error_msg)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +35,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'YOUR_SECRET_KEY'
+SECRET_KEY = get_private("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
