c94867475b39c30fd0530cdc638d5e464c9e27f6
Alfred-Walker <studio.alfred.walker@gmail.com>
2019-12-10 17:35:39 +0900

    Misc: Fix typo

--- studies/serializers.py
+++ studies/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Study
 
 
-class StudySerializers(serializers.ModelSerializer):
+class StudySerializer(serializers.ModelSerializer):
     class Meta:
         model = Study
         fields = '__all__'
