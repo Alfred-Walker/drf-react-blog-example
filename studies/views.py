1c2f1f562200fcd444a65841b2f8bbed33425419
Alfred-Walker <studio.alfred.walker@gmail.com>
2019-12-11 17:49:35 +0900

    Feat: Get public / private Study list

--- studies/views.py
+++ studies/views.py
@@ -1,13 +1,19 @@
 from django.db.models import Q
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+
 from .serializers import *
 from .models import Study
 
 
 class StudyViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    serializer_class = StudySerializer
 
+    # HTTP GET /study/list/
+    # HTTP GET /study/list/?search=
     def get_queryset(self):
         search = self.request.query_params.get('search', '')
 
@@ -26,4 +32,20 @@ class StudyViewSet(viewsets.ModelViewSet):
 
         return queryset
 
-    serializer_class = StudySerializer
+    # After DRF 3.8, list_route and detail_route have been merge into a single action decorator.
+    # Replace detail_route uses with @action(detail=True).
+    # Replace list_route uses with @action(detail=False).
+
+    # HTTP GET /study/list/public/
+    @action(detail=False)
+    def public(self, request):
+        queryset = Study.objects.filter(is_public=True).order_by('-registered_date')
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    # HTTP GET /study/list/private/
+    @action(detail=False)
+    def private(self, request):
+        queryset = self.get_queryset().filter(is_public=False)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
