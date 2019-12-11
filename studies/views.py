f7a660d04ebab6612cedcb9b51ef69e6d3234f66
Alfred-Walker <studio.alfred.walker@gmail.com>
2019-12-11 17:27:15 +0900

    Feat: Search Study via param named 'search'

--- studies/views.py
+++ studies/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import *
@@ -8,9 +9,21 @@ class StudyViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_queryset(self):
-        if self.request.user.is_superuser:
-            return Study.objects.all().order_by('-registered_date')
+        search = self.request.query_params.get('search', '')
+
+        if search:
+            if self.request.user.is_superuser:
+                criteria = (Q(title__icontains=search) | Q(body__icontains=search))
+            else:
+                criteria = ((Q(title__icontains=search) | Q(body__icontains=search)) & (Q(is_public=True) | Q(user=self.request.user)))
         else:
-            return Study.objects.filter(user=self.request.user).order_by('-registered_date')
+            if self.request.user.is_superuser:
+                criteria = Q()
+            else:
+                criteria = (Q(is_public=True) | Q(user=self.request.user))
+
+        queryset = Study.objects.filter(criteria).order_by('-registered_date')
+
+        return queryset
 
     serializer_class = StudySerializer
