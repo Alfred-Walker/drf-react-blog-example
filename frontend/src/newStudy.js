b0f004f7d8393bef7ae6eb2e9554770a391dc985
Alfred-Walker <studio.alfred.walker@gmail.com>
2019-12-26 20:59:52 +0900

    Add: Add dummy pages js

---
+++ frontend/src/newStudy.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Button, Form, Segment, Header, Label, List } from 'semantic-ui-react'
+
+
+function NewStudyForm() {
+    return (
+        <div className="form">
+            <Header as="h2">New Study</Header>
+            <Form>
+                <Form.Field>
+                    <label>Title</label>
+                    <input placeholder='Title' />
+                </Form.Field>
+                <Form.Field>
+                    <label>Contents</label>
+                    <Form.TextArea rows={10} placeholder='Contents' />
+                </Form.Field>
+                <Form.Field>
+                    <label>Tags</label>
+                    <Form.Input placeholder='Tags' />
+                </Form.Field>
+                <Form.Field>
+                    <Form.Checkbox label='Private' toggle/>
+                    <Form.Checkbox label='Notification' toggle/>
+                </Form.Field>
+                <List className="list-checkbox-horizontal">
+
+                </List>
+
+                <Button type='submit' color="blue">Submit</Button>
+            </Form>
+        </div>
+    )
+}
+export default NewStudyForm;
---
+++ frontend/src/studyInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Segment, Header, Grid, Image,Button } from 'semantic-ui-react'
+
+
+function StudyInfo() {
+    return (
+        <Grid centered columns={1} doubling>
+        Study Info (statistics) will be shown here
+        </Grid>
+    )
+}
+export default StudyInfo;
