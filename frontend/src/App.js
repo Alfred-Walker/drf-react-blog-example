c8e91fa10997485e6247d53ec8c9b5acd4126a86
Alfred-Walker <studio.alfred.walker@gmail.com>
2019-12-26 17:44:05 +0900

    Change: Setup dummy page data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//frontend/src/app.js
+import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+import ContactForm from './contact'
+import Login from './login';
+import NavigationBar from './navigation';
+import Studies from './studies';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        response: [],
+    };
+/*
+    async componentDidMount() {
+        try {
+            const res = await fetch('http://127.0.0.1:8000/users/');
+            const response = await res.json();
+            console.log(response)
+            this.setState({ response });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+*/
+    render() {
+
+    return (
+            <div className="App">
+                <NavigationBar />
+                <Studies />
+                <ContactForm />
+            </div >
+             /*
+             <div>
+             {
+             Object.keys(this.state.response).map((key) => {
+                const ret = <div>Key: {key}, Value: {key === "results" && this.state.response[key] ? this.state.response[key].map((result) => {
+                const ret = <div>{ result ? Object.keys(result).map((study) => {
+                    return result[study];
+                }) : "None" }</div>;
+                return ret;
+             })
+             : this.state.response[key]}</div>;
+             return ret;
+             })
+             }
+
+             </div>
+             */
+        );
+    }
 }
 
 export default App;
