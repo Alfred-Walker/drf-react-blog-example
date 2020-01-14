cf8daca6ba3ae5a0563f01976d2fc643f417ac0b
Alfred-Walker <studio.alfred.walker@gmail.com>
2020-01-14 19:29:50 +0900

    Change: Apply tagsInput as the field

--- frontend/src/components/study/newStudy.js
+++ frontend/src/components/study/newStudy.js
@@ -1,35 +1,160 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Button, Form, Header, List } from 'semantic-ui-react'
+import TagsInput from 'react-tagsinput';
+import './newStudy.css'
+import * as helpers from '../helpers/jwt'
 
+/* react-tagsinput reference */
+/* https://github.com/olahol/react-tagsinput */
 
-function NewStudyForm(props) {
-    return (
-        <div className="form">
-            <Header as="h2">New Study</Header>
-            <Form>
-                <Form.Field>
-                    <label>Title</label>
-                    <input placeholder='Title' />
-                </Form.Field>
-                <Form.Field>
-                    <label>Contents</label>
-                    <Form.TextArea rows={10} placeholder='Contents' />
-                </Form.Field>
-                <Form.Field>
-                    <label>Tags</label>
-                    <Form.Input placeholder='Tags' />
-                </Form.Field>
-                <Form.Field>
-                    <Form.Checkbox label='Private' toggle/>
-                    <Form.Checkbox label='Notification' toggle/>
-                </Form.Field>
-                <List className="list-checkbox-horizontal">
+class NewStudy extends Component {
+    constructor(props) {
+        super(props);
 
-                </List>
+        this.state = {
+            title: "",
+            body: "",
+            tags: [],
+            is_public: true,
+            notification_enabled: false,
+            review_cycle_in_minute: 12
+        }
 
-                <Button type='submit' color="blue">Submit</Button>
-            </Form>
-        </div>
-    )
+        this.handleChange = this.handleChange.bind(this);
+        this.handleTagsChange = this.handleTagsChange.bind(this);
+        this.handleToggleChange = this.handleToggleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleTagsChange(tags) {
+        this.setState({tags});
+    }
+
+    handleToggleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.checked
+        });
+    }
+
+    handleSubmit(event) {
+        const jwt = helpers.getJwt();
+
+        const {
+            title,
+            body,
+            tags,
+            is_public,
+            notification_enabled,
+            review_cycle_in_minute
+        } = this.state;
+        console.log(tags);
+        console.log(JSON.stringify({
+            title: title,
+            body: body,
+            tags: tags,
+            is_public: is_public,
+            notification_enabled: notification_enabled,
+            review_cycle_in_minute: review_cycle_in_minute
+        }));
+        event.preventDefault();
+
+        fetch(
+            'http://localhost:8000/study/', {
+            method: 'POST',
+            headers: {
+                'Authorization': `JWT ${jwt}`,
+                'Content-Type': 'application/json; charset="utf-8"'
+            },
+            body: JSON.stringify({
+                title: title,
+                body: body,
+                tags: tags,
+                is_public: is_public,
+                notification_enabled: notification_enabled,
+                review_cycle_in_minute: review_cycle_in_minute
+            }),
+            credentials: 'include'
+        }
+        )
+            .then(
+                response => (response.json())
+            )
+            .then(
+                result => {
+                    this.props.history.push('list');
+                }
+            )
+            .catch(
+                err => console.log("login error", err)
+            );
+
+        // POST API here
+    }
+
+    render() {
+        return (
+            <div className="form">
+                <Header as="h2">New Study</Header>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Field>
+                        <label>Title</label>
+                        <input
+                            name='title'
+                            placeholder='Title'
+                            value={this.state.title}
+                            onChange={this.handleChange}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Contents</label>
+                        <Form.TextArea
+                            name='body'
+                            rows={10}
+                            placeholder='Contents'
+                            value={this.state.body}
+                            onChange={this.handleChange}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Tags</label>
+                        <TagsInput
+                            name='tagsInput'
+                            value={this.state.tags}
+                            onChange={this.handleTagsChange}
+                            addKeys={[9, 13, 188]}
+                            onlyUnique={true} />
+                    </Form.Field>
+                    <Form.Field>
+                        <Form.Checkbox
+                            name='is_public'
+                            defaultChecked={this.state.is_public}
+                            onChange={this.handleToggleChange}
+                            label='Is Public'
+                            toggle
+                        />
+                        <Form.Checkbox
+                            name='notification_enabled'
+                            defaultChecked={this.state.notification_enabled}
+                            onChange={this.handleToggleChange}
+                            label='Notification Enabled'
+                            toggle
+                        />
+                    </Form.Field>
+                    <List className="list-checkbox-horizontal">
+
+                    </List>
+
+                    <Button type='submit' color="blue">Submit</Button>
+                </Form>
+            </div>
+        )
+    }
 }
-export default NewStudyForm;
+
+export default NewStudy;
--- frontend/src/components/study/studies.js
+++ frontend/src/components/study/studies.js
@@ -1,30 +1,112 @@
-import React from 'react';
-import { Button, Grid, Header, Label, List, Segment} from 'semantic-ui-react'
-import { studies } from './dummy-studies';
+import React, { Component } from 'react';
+import { Button, Grid, Header, List, Segment, Label } from 'semantic-ui-react';
+import { getJwt } from '../helpers/jwt';
+import TagsInput from 'react-tagsinput';
+import './studies.css';
 
 
-function Studies(props) {
-    return (
-        <Grid centered columns={1} doubling> {
-                    studies.map(study =>
-                    <Grid.Column key={study.title}>
+class Studies extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            index: 0,
+            studyList: undefined,
+            tags: [],
+            suggestions: []
+        }
+
+        this.tagsInputProps = {
+            className: 'react-tagsinput-input',
+            placeholder: ''
+          }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(tags) {
+        this.setState({ tags });
+    }
+
+    componentDidMount() {
+        let headers = {};
+        const jwt = getJwt();
+
+        if (jwt) {
+            headers = { 
+                'Authorization': `JWT ${jwt}`,
+                'Content-Type': 'Application/json'
+            };
+        }
+
+        const studyListUrl = this.props.studyListUrl;
+
+        fetch(
+            studyListUrl, {
+            method: 'GET',
+            headers: headers,
+            credentials: 'include'
+        }
+        )
+            .then(
+                response => (response.json())
+            )
+            .then(
+                result => {
+                    this.setState({
+                        studyList: result
+                    });
+                }
+            )
+            .then(
+                console.log(this.state)
+            )
+            .catch(
+                // TODO: need better catch.
+                err => {
+                    console.log("studies error");
+                    console.log(err);
+                    this.props.history.push('/');
+                }
+            );
+    }
+
+    render() {
+        if (this.state.studyList === undefined) {
+            return (
+                <div><h1>No studies found...</h1></div>
+            );
+        }
+
+        return (
+            <Grid centered columns={1} doubling> {
+                this.state.studyList.results.map(study =>
+                    <Grid.Column key={study.id}>
                         <Segment>
-                            <Header as="h1">{study.title}</Header>
-                            <p>{study.date}</p>
+                            <Header as="h1">
+                                {study.title}{study.is_public ? "" : <Label className="ui horizontal red">Private</Label>}
+                            </Header>
+                            <p>{study.body}</p>
+                            <p>
+                            {study.registered_date}
+                            </p>
                             <p>{study.excerpt}</p>
                             <List className="list-tag-horizontal">
-                                <Label tag>tag a</Label>
-                                <Label tag>tag b</Label>
-                                <Label tag>tag c</Label>
-                                <Label tag>tag d</Label>
+                                <TagsInput in
+                                disabled={true} 
+                                value={study.tags}
+                                inputProps={this.tagsInputProps}
+                                />
                             </List>
                             <Button primary basic as="a" href="/">See all</Button>
                             <Button primary basic as="a" href="/">Edit</Button>
                         </Segment>
                     </Grid.Column>
-                    )
-                }
-        </Grid>
-    )
+                )
+            }
+            </Grid>
+        )
+    }
 }
+
 export default Studies;
