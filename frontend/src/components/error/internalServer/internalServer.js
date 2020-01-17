c93662090e7e32b38e6515974065d887ca98c655
Alfred-Walker <studio.alfred.walker@gmail.com>
2020-01-17 21:05:11 +0900

    Change: Change error page style

--- frontend/src/components/error/internalServer/internalServer.js
+++ frontend/src/components/error/internalServer/internalServer.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Button, GridColumn, Divider, Label, Message } from 'semantic-ui-react'
+import { Button, Divider, GridColumn, Header, Label, Message } from 'semantic-ui-react'
 import './internalServer.css';
 
 
 const internalServer = (props) => {
     return (
-        <GridColumn className='_error' >
-            <div class='_code' > 500 </div> 
-            <br />
-            <Label className='_1' > INTERNAL SERVER ERROR </Label> 
+        <GridColumn className='internal-server' >
+            <Header size='huge' className='header' > 500 </Header> 
+            <Label className='label' > INTERNAL SERVER ERROR </Label> 
             <Divider />
-            <Message className='_2 negative' > 
+            <Message className='message negative' > 
                 Sorry, the requested url was not found on this server. <br />
                 If the problem persists, please contact to administrator.
              </Message> 
-            <Button className='btn' onClick={props.history.goBack} > BACK </Button> 
+            <Button className='button' onClick={props.history.goBack} primary basic> BACK </Button> 
         </GridColumn> 
     )
 }
--- frontend/src/components/error/notFound/notFound.js
+++ frontend/src/components/error/notFound/notFound.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Button, GridColumn, Divider, Label, Message } from 'semantic-ui-react'
+import { Button, Divider, GridColumn, Header, Label, Message } from 'semantic-ui-react'
 import './notFound.css';
 
 
 const notFound = (props) => {
     return (
-        <GridColumn className='_error' >
-            <div class='_code' > 404 </div> 
-            <br />
-            <Label className='_1' > PAGE NOT FOUND </Label> 
-            <Divider />
-            <Message className='_2 negative' > Sorry, the requested url was not found on this server. </Message> 
-            <Button className='btn' onClick={props.history.goBack} > BACK </Button> 
+        <GridColumn className='not-found' >
+                <Header size='huge' className='header' > 404 </Header> 
+                <Label className='label' > PAGE NOT FOUND </Label> 
+                <Divider />
+                <Message className='message negative' > Sorry, the requested url was not found on this server. </Message> 
+                <Button className='button' onClick={props.history.goBack} primary basic> BACK </Button> 
         </GridColumn> 
     )
 }
