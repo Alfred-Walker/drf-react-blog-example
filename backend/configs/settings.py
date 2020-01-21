3f09792a51700522b6ca8fcb653c594b991e5ace
Alfred-Walker <studio.alfred.walker@gmail.com>
2020-01-21 20:15:36 +0900

    Change: Change default pagination class

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -188,7 +188,8 @@ REST_FRAMEWORK = {
         # 'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2,
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.AutoSchema',
 }
 
