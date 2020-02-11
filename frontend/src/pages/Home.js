93bbf5efbcd45b2cca72bb065de73084af5b89e4
Alfred-Walker <studio.alfred.walker@gmail.com>
2020-02-11 18:31:01 +0900

    Change: Apply components for 'Home' page with dummy data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,59 +1,81 @@
 import React from 'react';
-import { Button, Container, Divider, Grid, Header, Icon, Image, List, Segment, Transition } from 'semantic-ui-react'
-import TagList from './tag/TagList';
+import {
+    Container, 
+    Divider, 
+    Grid, 
+    Segment
+} from 'semantic-ui-react'
+
+import LastStudy from './home/LastStudy'
+import LastQuestion from './home/LastQuestion'
+import LatestStudy from './home/LatestStudy'
+import LatestQuestion from './home/LatestQuestion'
+import PopularTags from './home/PopularTags';
+import CountInfo from './home/CountInfo';
 
 
-function Home() {
+function Home(props) {
+    const dummy_study = {
+        id: 1,
+        title: "dummy_study_title",
+        body: "dummy_study_body",
+        created_date: "2020-02-02"
+    }
+
+    const dummy_question = {
+        id: 1,
+        title: "dummy_question_title",
+        body: "dummy_question_body",
+        created_date: "2020-02-02"
+    }
+
+    const dummy_popular_tags = [
+        { name: "django", count: 123456 },
+        { name: "react", count: 123 },
+        { name: "semantic-ui", count: 12345 },
+        { name: "python", count: 123456 },
+        { name: "javascript", count: 12345 },
+    ]
+
+    const dummy_user_count = 123456;
+    const dummy_study_count = 123456;
+    const dummy_question_count = 123456;
+    const dummy_comment_count = 123456;
+
     return (
         <Container>
             <Segment style={{ padding: '3em 0em 8em 0em', border: 'none' }} vertical>
                 <Grid container stackable verticalAlign='middle'>
+                    {
+                        props.loggedInStatus === 'LOGGED_IN' ?
+                        <Grid.Row>
+                        <Grid.Column width={8}>
+                            <LastStudy 
+                                study={dummy_study} 
+                                study_link_path='/study/'
+                            />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <LastQuestion
+                                question={dummy_question} 
+                                question_link_path='/question/'
+                            />
+                        </Grid.Column>
+                    </Grid.Row> : ""       
+                    }
+                    <Grid.Row></Grid.Row>
                     <Grid.Row>
                         <Grid.Column width={8}>
-                            <Header as='h3' style={{ fontSize: '2em' }}>
-                                Last Study (LOGGINED_USER)
-                        </Header>
-                            <Divider />
-                            <p style={{ fontSize: '1.33em' }}>
-                                We can give your company superpowers to do things that they never thought possible.
-                                Let us delight your customers and empower your needs... through pure data analytics.
-                        </p>
-                            <Button>More</Button>
+                            <LatestStudy 
+                                study={dummy_study} 
+                                study_link_path='/study/'
+                            />
                         </Grid.Column>
                         <Grid.Column width={8}>
-                            <Header as='h3' style={{ fontSize: '2em' }}>
-                                Last Question (LOGGINED_USER)
-                        </Header>
-                            <Divider />
-                            <p style={{ fontSize: '1.33em' }}>
-                                We can give your company superpowers to do things that they never thought possible.
-                                Let us delight your customers and empower your needs... through pure data analytics.
-                        </p>
-                            <Button>More</Button>
-                        </Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Header as='h3' style={{ fontSize: '2em' }}>
-                                Latest Study by author
-                        </Header>
-                            <Divider />
-                            <p style={{ fontSize: '1.33em' }}>
-                                We can give your company superpowers to do things that they never thought possible.
-                                Let us delight your customers and empower your needs... through pure data analytics.
-                        </p>
-                            <Button>More</Button>
-                        </Grid.Column>
-                        <Grid.Column width={8}>
-                            <Header as='h3' style={{ fontSize: '2em' }}>
-                                Latest Question by author
-                        </Header>
-                            <Divider />
-                            <p style={{ fontSize: '1.33em' }}>
-                                We can give your company superpowers to do things that they never thought possible.
-                                Let us delight your customers and empower your needs... through pure data analytics.
-                        </p>
-                            <Button>More</Button>
+                            <LatestQuestion 
+                                question={dummy_question} 
+                                question_link_path='/question/'
+                            />
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
@@ -66,76 +88,22 @@ function Home() {
             >
                 <a href='#'>Popular Tags</a>
             </Divider>
-            <Segment style={{ padding: '0em 0em 4em 0em', border: 'none' }} vertical>
+            <Segment style={{ padding: '2em 0em 2em 0em', border: 'none' }} vertical>
                 <Segment style={{ padding: '0em' }} vertical>
-                    <Grid celled='internally' columns='equal' stackable>
-                        <Grid.Row textAlign='center'>
-                            <Grid.Column style={{ paddingBottom: '1em', paddingTop: '1em' }}>
-                                <Header as='h3' style={{ fontSize: '1.5em' }}>
-                                    django
-                            </Header>
-                                <p style={{ fontSize: '1.33em' }}>x123,456</p>
-                            </Grid.Column>
-                            <Grid.Column style={{ paddingBottom: '1em', paddingTop: '1em' }}>
-                                <Header as='h3' style={{ fontSize: '1.5em' }}>
-                                    react
-                            </Header>
-                                <p style={{ fontSize: '1.33em' }}>x123</p>
-                            </Grid.Column>
-                            <Grid.Column style={{ paddingBottom: '1em', paddingTop: '1em' }}>
-                                <Header as='h3' style={{ fontSize: '1.5em' }}>
-                                    semantic-ui
-                            </Header>
-                                <p style={{ fontSize: '1.33em' }}>x123</p>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                </Segment>
-                <Segment style={{ padding: '0em 0em 0em 0em' }} vertical>
-                    <Grid celled='internally' columns='equal' stackable>
-                        <Grid.Row textAlign='center'>
-                            <Grid.Column style={{ paddingBottom: '1em', paddingTop: '1em' }}>
-                                <Header as='h3' style={{ fontSize: '1.5em' }}>
-                                    python
-                            </Header>
-                                <p style={{ fontSize: '1.33em' }}>x123</p>
-                            </Grid.Column>
-                            <Grid.Column style={{ paddingBottom: '1em', paddingTop: '1em' }}>
-                                <Header as='h3' style={{ fontSize: '1.5em' }}>
-                                    javascript
-                            </Header>
-                                <p style={{ fontSize: '1.33em' }}>x123</p>
-                            </Grid.Column>
-                            <Grid.Column style={{ paddingBottom: '1em', paddingTop: '1em' }}>
-                                <Header as='h3' style={{ fontSize: '1.5em' }}>
-                                    http
-                            </Header>
-                                <p style={{ fontSize: '1.33em' }}>x123</p>
-                            </Grid.Column>
-                            <Grid.Column style={{ paddingBottom: '1em', paddingTop: '1em' }}>
-                                <Header as='h3' style={{ fontSize: '1.5em' }}>
-                                    mysql
-                            </Header>
-                                <p style={{ fontSize: '1.33em' }}>x123</p>
-                            </Grid.Column>
-                            <Grid.Column style={{ paddingBottom: '1em', paddingTop: '1em' }}>
-                                <Header as='h3' style={{ fontSize: '1.5em' }}>
-                                    git
-                            </Header>
-                                <p style={{ fontSize: '1.33em' }}>x123</p>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
+                    <PopularTags 
+                        tags={dummy_popular_tags} 
+                        tag_link_path="/tag/"
+                    />
                 </Segment>
             </Segment>
 
             <Segment textAlign='center'>
-                <List horizontal>
-                    <List.Item><Icon name='user' /> User: 123,456</List.Item>
-                    <List.Item><Icon name='book' /> Study: 123,456</List.Item>
-                    <List.Item><Icon name='question' /> Question: 123,456</List.Item>
-                    <List.Item><Icon name='comment' /> Comment: 123,456</List.Item>
-                </List>
+                <CountInfo
+                    user_count={dummy_user_count}
+                    study_count={dummy_study_count}
+                    question_count={dummy_question_count}
+                    comment_count={dummy_comment_count}
+                />
             </Segment>
         </Container>
     )
