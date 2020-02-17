0871057890313347b4e6ad0859d9c8442dff9f7f
Alfred-Walker <studio.alfred.walker@gmail.com>
2020-02-17 22:13:48 +0900

    Add: Add new responses (last, latest questions)

--- backend/questions/views.py
+++ backend/questions/views.py
@@ -1,6 +1,8 @@
 from django.db.models import Q
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework.response import Response
 
 from .serializers import *
 from .models import Question
@@ -32,3 +34,24 @@ class QuestionViewSet(viewsets.ModelViewSet):
             queryset = Question.objects.filter(criteria).order_by('-registered_date')
 
         return queryset
+
+    # HTTP GET /question/last/
+    @action(detail=False)
+    def last(self, request):
+        """
+            Most recent question written by request user
+        """
+        queryset = self.get_queryset().filter(user=self.request.user).order_by('-registered_date').first()
+        serializer = self.get_serializer(queryset, many=False)
+        return Response(serializer.data)
+
+    # HTTP GET /question/latest/
+    @action(detail=False)
+    def latest(self, request):
+        """
+            Latest registered question
+        """
+        # queryset = self.get_queryset().order_by('-registered_date')[:1]
+        queryset = self.get_queryset().first()
+        serializer = self.get_serializer(queryset, many=False)
+        return Response(serializer.data)
