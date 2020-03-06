30385c36fba3e9704eabdfd4effa3072ce5197d7
Alfred-Walker <studio.alfred.walker@gmail.com>
2020-03-06 17:55:15 +0900

    Feat: Support study/question action with customized pagination

--- backend/tags/views.py
+++ backend/tags/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Count, F, Q
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework.response import Response
 
@@ -14,10 +15,18 @@ from questions.serializers import QuestionSummarySerializer
 from itertools import chain
 
 
+class TagPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100000
+
+
 class TagViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly,)
     # permission_classes = (AllowAny,)
     serializer_class = TagSerializer
+
+    pagination_class = TagPagination
 
     # HTTP GET /tag/
     # HTTP GET /tag/?search=
@@ -81,3 +90,45 @@ class TagViewSet(viewsets.ModelViewSet):
             return Response({"study": study_serializer.data, "question": question_serializer.data})
         else:
             return Response(chain([{"study": []}], [{"question": []}]))
+
+    # HTTP GET /tag/study/
+    @action(detail=False)
+    def study(self, request):
+        tag_name = self.request.query_params.get('tag', '')
+        tag = self.get_queryset().get(name=tag_name, is_public=True)
+
+        queryset = None
+
+        if tag:
+            queryset = tag.studies.all().order_by('-registered_date')
+
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+            serializer = StudySummarySerializer(page, many=True, context={'request': request})
+            return self.get_paginated_response(serializer.data)
+
+        serializer = StudySummarySerializer(many=True, context={'request': request})
+
+        return Response(serializer.data)
+
+    # HTTP GET /tag/question/
+    @action(detail=False)
+    def question(self, request):
+        tag_name = self.request.query_params.get('tag', '')
+        tag = self.get_queryset().get(name=tag_name, is_public=True)
+
+        queryset = None
+
+        if tag:
+            queryset = tag.questions.all().order_by('-registered_date')
+
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+            serializer = QuestionSummarySerializer(page, many=True, context={'request': request})
+            return self.get_paginated_response(serializer.data)
+
+        serializer = QuestionSummarySerializer(many=True, context={'request': request})
+
+        return Response(serializer.data)
